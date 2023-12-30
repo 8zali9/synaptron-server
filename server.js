bac235075f0bc4481f7664f2027d66ae1315aff7
Zulfiqar Ali <8zali9@gmail.com>
2023-12-30 22:35:29 +0500

    Converted into Chat. Configuration remaining.

--- server.js
+++ server.js
@@ -19,13 +19,31 @@ app.use(
 );
 
 app.route("/").get(async (req, res) => {
-  const { prompt } = req.body;
+  const { msg } = req.body;
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-  if (!prompt) {
-    return res.status(404).send({ error: "No prompt provided." });
+  if (!msg) {
+    return res.status(404).send({ error: "No msg provided." });
   } else {
-    const result = await model.generateContent(prompt);
+    const chat = model.startChat({
+      history: [
+        {
+          role: "user",
+          parts: "Hello, I have something to ask or say.",
+        },
+        {
+          role: "model",
+          parts: "Great to meet you. What would you like to know?",
+        },
+      ],
+      generationConfig: {
+        maxOutputTokens: 10000,
+      },
+    });
+
+    console.log(chat.getHistory());
+
+    const result = await chat.sendMessage(msg);
     const response = await result.response;
     const text = response.text();
     return res.send(text);
